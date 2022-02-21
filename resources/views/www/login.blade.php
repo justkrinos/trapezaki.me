0a2cb9bed4430fab3379d2be02badc35381565bc
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-02-21 20:46:23 +0100

    login is now fully in blade and uses popup messages

--- resources/views/www/login.blade.php
+++ resources/views/www/login.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trapezaki - Login</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+
+    {{--Toast dependency--}}
+    <link rel="stylesheet" href="assets/vendors/toastify/toastify.css">
+
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="../assets/css/app.css">
@@ -19,38 +23,62 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="/profile"><img src="../assets/images/logo/logo.png" alt="Trapezaki"></a>
+                        <a href="/"><img src="../assets/images/logo/logo.png" alt="Trapezaki"></a>
                     </div>
                     <h1 class="auth-title">Log in.</h1>
 
 
-                    <form action="/profile">
+                    <form method="POST" action="/login">
+                        @csrf
+
+                        @error('message')
+                        <input class="is-invalid text-center" hidden>
+                        <div class="invalid-feedback">
+                            <i class="bx bx-radio-circle"></i>
+                            {{ $message }}
+                        </div>
+                         @enderror
+
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Username">
+                            <input type="text" class="form-control form-control-xl" placeholder="Username" name="username" id="username">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                             </div>
                         </div>
+
+
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password" name="password" id="password">
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
                         </div>
+
+
                         <div class="form-check form-check-lg d-flex align-items-end">
                             <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+
+                            {{--TODO: na dume an tha to kratisume to keep me logged in--}}
                             <label class="form-check-label text-gray-600" for="flexCheckDefault">
                                 Keep me logged in
                             </label>
                         </div>
+
                         <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-                        <a href="/signup" class='btn btn-primary btn-block btn-lg shadow-lg mt-5'>
-                                <span>Sign up</span>
-                        </a>
-                        <a href="/guest" class='btn btn-primary btn-block btn-lg shadow-lg mt-5'>
-                            <span>Continue as a guest</span>
-                        </a>
+
+
+                        <div class="col-12 col-md-10 m-3">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-center">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">Don't have an account? <a href="/signup">Signup</a></li>
+                                </ol>
+                            </nav>
+                        </div>
+
+
                     </form>
+
+
                 </div>
             </div>
             <div class="col-lg-7 d-none d-lg-block">
@@ -64,3 +92,10 @@
 </body>
 
 </html>
+
+{{--Toast dependencies--}}
+<script src="assets/vendors/toastify/toastify.js"></script>
+<script src="assets/js/extensions/toastify.js"></script>
+<script src="../assets/js/jquery-3.6.0.min.js"></script>
+
+@include("www.components.toasts");
