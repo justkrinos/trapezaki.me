1267a5b363d5b18db56e51b1d8c1c3312e0c7c8b
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-02-21 20:46:59 +0100

    integration with blade (sidebar and navbar)

---
+++ resources/views/www/components/sidebar.blade.php
@@ -0,0 +1,53 @@
+<div id="sidebar" class="">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="d-flex justify-content-center">
+                <div class="logo">
+                    <a href="/"><img src="../assets/images/logo/logo.png" alt="Trapezaki"
+                            srcset=""></a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item
+                @if( request()->route()->uri === "profile")
+                active
+                @endif
+                ">
+                    <a href="/profile" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>My Profile</span>
+                    </a>
+                </li>
+                <li class="sidebar-item
+                @if( request()->route()->uri === "my-reservations")
+                active
+                @endif
+                ">
+                    <a href="/my-reservations" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>My Reservations</span>
+                    </a>
+                </li>
+                <li class="sidebar-item
+                @if( request()->route()->uri === "make-a-reservation")
+                active
+                @endif
+                ">
+                    <a href="/make-a-reservation" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Make a Reservation</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
--- resources/views/www/reservations.blade.php
+++ resources/views/www/reservations.blade.php
@@ -23,101 +23,13 @@
 
 <body>
     <div id="app">
-        <div id="sidebar" class="">
-            <div class="sidebar-wrapper active">
-                <div class="sidebar-header">
-                    <div class="d-flex justify-content-center">
-                        <div class="logo">
-                            <a href="/"><img src="../assets/images/logo/logo.png" alt="Trapezaki"
-                                    srcset=""></a>
-                        </div>
-                        <div class="toggler">
-                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="sidebar-menu">
-                    <ul class="menu">
-                        <li class="sidebar-title">Menu</li>
 
-                        <li class="sidebar-item">
-                            <a href="/profile" class='sidebar-link'>
-                                <i class="bi bi-grid-fill"></i>
-                                <span>My Profile</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item active">
-                            <a href="/my-reservations" class='sidebar-link'>
-                                <i class="bi bi-grid-fill"></i>
-                                <span>My Reservations</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-item ">
-                            <a href="/make-a-reservation" class='sidebar-link'>
-                                <i class="bi bi-grid-fill"></i>
-                                <span>Make a Reservation</span>
-                            </a>
-                        </li>
-
-                    </ul>
-                </div>
-                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
-            </div>
-        </div>
+        @include("www.components.sidebar")
 
         <div id="main" class='layout-navbar'>
-            <header class='mb-3'>
-                <nav class="navbar navbar-expand">
-                    <div class="container-fluid">
-                        <a href="#" class="burger-btn d-block">
-                            <i class="bi bi-justify fs-3"></i>
-                        </a>
 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
+            @include("www.components.navbar")
 
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto mb-0 mb-lg-0">
-                                <div class="user-menu align-items-center">
-                                    <div class="user-name text-center me-3">
-                                        <h6 class="mb-0 text-gray-600 text-nowrap">
-                                            <a href="/make-a-reservation">Make a reservation</a>
-                                        </h6>
-                                    </div>
-                                </div>
-                            </ul>
-                            <div class="dropdown">
-                                <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <div class="user-menu d-flex">
-                                        <div class="user-name text-end me-3">
-                                            <h6 class="mb-0 text-blue">@myusername</h6>
-                                        </div>
-                                    </div>
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                                    <li>
-                                        <h6 class="dropdown-header">Hello, &lt;Name&gt;!</h6>
-                                    </li>
-                                    <li><a class="dropdown-item" href="/profile"><i
-                                                class="icon-mid bi bi-person me-2"></i> My
-                                            Profile</a></li>
-                                    <li><a class="dropdown-item" href="/my-reservations"><i
-                                                class="icon-mid bi bi-wallet me-2"></i>
-                                            My Reservations</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#"><i
-                                                class="icon-mid bi bi-box-arrow-left me-2"></i> Logout</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </header>
             <div id="main-content">
 
                 <div class="page-heading">
