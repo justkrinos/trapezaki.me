079949b6b8470203b477ae0a77be96bac506f883
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-02-21 20:44:04 +0100

    User3 Model now has password authenticatio method

--- app/Models/User3.php
+++ app/Models/User3.php
@@ -1,11 +1,29 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User3 extends Model
+class User3 extends Authenticatable
 {
     use HasFactory;
+
+    //Gia na men exume thema me ta fillable j mass assignments
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password',
+        'password_ver'
+    ];
+
+    //Opote ena kamw set password, prin na mpei mesto database enna kamei run to function
+    //iparxei kai to antistoixo get gia opote kamw access pu to database
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
