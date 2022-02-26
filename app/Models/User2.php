f1274887caad3fb5cb60b73e9fc3ef2e6ea3832a
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2022-02-26 11:40:31 +0200

    Updates the User2 model

--- app/Models/User2.php
+++ app/Models/User2.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class User2 extends Model
 {
     use HasFactory;
+
+    protected $guard = 'user2';
+
+    //Can mass assignment-fillables
+
+    protected $hidden = [
+        'password',
+        'password_ver'
+    ];
+
+    //bcrypt = password encryption function
+    public function setPasswordAttribute($password){
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
