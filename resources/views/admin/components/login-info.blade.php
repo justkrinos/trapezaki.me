86be931d5a55bc30c2322889c6773158aa5c9b2f
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-03-07 19:47:24 +0100

    admin can now log out using sidebar button

---
+++ resources/views/admin/components/login-info.blade.php
@@ -0,0 +1,21 @@
+<div class="d-flex justify-content-center">
+<div class="dropdown">
+    <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
+        <div class="user-menu d-flex">
+
+            <div class="user-name text-end me-3">
+                <h6 class="mb-0 text-blue">{{Auth::guard('user1')->user()->username;}}</h6>
+            </div>
+
+        </div>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+        <li>
+            <a class="dropdown-item" href="/logout">
+                <i class="icon-mid bi bi-box-arrow-left me-2"></i>
+                Logout
+            </a>
+        </li>
+    </ul>
+</div>
+</div>
