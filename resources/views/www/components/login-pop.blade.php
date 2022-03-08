0806de483eea3a575329a4ab3ca6d7ed2f6702a0
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-03-08 12:11:33 +0100

    login components for booking popup

---
+++ resources/views/www/components/login-pop.blade.php
@@ -0,0 +1,21 @@
+<form action="#">
+
+    <div class="modal-body">
+        <label>Username: </label>
+        <div class="form-group">
+            <input type="text" placeholder="Username or Email" id="username" class="form-control">
+        </div>
+        <label>Password: </label>
+        <div class="form-group">
+            <input type="password" placeholder="Password" id="password" class="form-control">
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+            <span class="d-sm-block">Close</span>
+        </button>
+        <button type="button submit" id="btnLogn" class="btn btn-primary ml-1">
+            <span class="d-sm-block">Login</span>
+        </button>
+    </div>
+</form>
