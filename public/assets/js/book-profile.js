1b19299fde6e63898bbc20045f9e853a00ac6a4c
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-03-08 12:11:11 +0100

    popup comments added to be done

--- public/assets/js/book-profile.js
+++ public/assets/js/book-profile.js
@@ -1,20 +1,53 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    if (window.location.hash.match("login")) {
+        $("#inlineForm").modal('show')
+    }
 
     //Go back button
-    $("#btnBack").click(function() {
+    $("#btnBack").click(function () {
         history.go(-1)
     })
 
-    $("#btnBook").click(function() {
-        //TODO: check if logged in
-        // if not then open modal
-        $("#inlineForm").modal('show')
-        
-        //TODO: after log in redirect
+    //An ise logged in fkenni tunto button
+    $("#btnBook").click(function () {
+        window.open('/seven-seas/book');
     })
 
-    $("#resvMenu").click(function(){
+    //An den ise logged in fkenni tunto button
+    $("#btnPop").click(function () {
+        $("#inlineForm").modal('show')
+    })
+
+    $("#resvMenu").click(function () {
         alert("TODO");
         window.open("/TODO", '_blank').focus();
     })
+
+    // $("#btnLogin").click(function (e) {
+    //     e.preventDefault();
+    //     $.ajaxSetup({
+    //         headers: {
+    //             'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+    //         }
+    //     });
+    //     alert("ok");
+    //     $.ajax({
+    //         url: "/login_popup",
+    //         method: 'post',
+    //         data: {
+    //             username: $('#username').val(),
+    //             password: $('#password').val()
+    //         },
+    //         success: function (result) {
+    //             console.log(result);
+    //         }
+    //     });
+
+    // });
+
+    //Post sto /login
+    //when success -> redirect sto /seven-seas/book
+    //when fail    -> return fail message stin idia selida xoris refresh
+
+
 })
