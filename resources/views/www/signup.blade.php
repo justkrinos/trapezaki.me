9cbd6a64f9625df0e29380300de78ab2294c101b
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-03-08 12:12:46 +0100

    sidebar button removed

--- resources/views/www/signup.blade.php
+++ resources/views/www/signup.blade.php
@@ -23,11 +23,6 @@
 <body>
     <div id="app">
         <div id="main">
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
-            </header>
 
             <div class="page-heading">
                 <div class="page-title">
