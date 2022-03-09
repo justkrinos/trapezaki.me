4c75be9287383ca27a600fef00d283fce2eb5d53
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-03-09 16:52:51 +0100

    made this demo page gia na kserume pws na to kamume meta

--- resources/views/www/book.blade.php
+++ resources/views/www/book.blade.php
@@ -1,2 +1,24 @@
-TODO
-kapu to exume misokamoumeno but en to girepsa
+TODO kapu to exume misokamoumeno tunto page but en to girepsa
+TODO ta session variables pu katw en tha fenunte stin forma, enna pernoun apefthias ston controller opws to ekama
+TODO ta mona p ena eshi stin forma ennan ta stoixeia tis kratisis
+
+<br>
+<br>
+
+Name: {{ session()->get('full_name')}}
+<br>
+
+Phone: {{ session()->get('phone')}}
+
+<br>
+
+Email: {{ session()->get('email')}}
+<br>
+<form method="POST" action="/seven-seas/book">
+@csrf
+<button type="submit">Submit Booking</button>
+</form>
+
+<br>
+<br>
+TODO: Submit popup successfull j otan kamnis click ok na ginete redirect piso sto make a reservation
