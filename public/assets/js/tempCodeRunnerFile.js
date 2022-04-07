ebc546b586d7ad5d0daec05cfa13b380caa4eb9f
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-04-07 14:45:46 +0200

    deleted temporary file

--- public/assets/js/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-$description = $(this).siblings(".issue-description").html();
-        $issuer = $(this).siblings(".issue-business").html();
-        $type = $(this).siblings(".issue-type").html();
-
-        $("#issueTextArea").html($description);
-        $("#issueBusiness").html($issuer);
-        $("#issueType").html($type);
