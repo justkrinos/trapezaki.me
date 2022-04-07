391b42005ba181de7950af27ba6b84dfd471e75f
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-04-07 21:49:37 +0200

    max size added to cards, to searchbar egine component

---
+++ resources/views/www/components/searchbar.blade.php
@@ -0,0 +1,62 @@
+<section class="section">
+    <div class="card">
+        <div class="card-header bg-primary">
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+                <input type="text" class="form-control" placeholder="Find restaurants, bar, cafe..."
+                    aria-describedby="button-addon2">
+
+            </div>
+            <div class="card-content bg-primary">
+                <div class="row">
+
+                    <div class="col-md-4 row-col-4 mb-3">
+                        <h6 class="text-white">Date</h6>
+                        <input type="date" id="mydate" class="form-control" value="2017-06-01">
+                    </div>
+
+                    <div class="col-md-2 mb-2">
+                        <h6 class="text-white">People </h6>
+                        <fieldset class="form-group">
+                            <select class="form-select" id="basicSelect">
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                            </select>
+                        </fieldset>
+                    </div>
+                    <div class="col-md-3 mb-2">
+                        <h6 class="text-white">Category </h6>
+                        <fieldset class="form-group">
+                            <select class="form-select" id="basicSelect">
+                                <option>Food </option>
+                                <option> Drink </option>
+                                <option> Coffee </option>
+                            </select>
+                        </fieldset>
+                    </div>
+
+                    <div class="col-md-3 mb-2">
+                        <h6 class="text-white">City</h6>
+                        <fieldset class="form-group">
+                            <select class="form-select" id="basicSelect">
+                                <option>Limassol</option>
+                                <option>Paphos</option>
+                                <option>Larnaca</option>
+                                <option>Nicosia</option>
+                                <option>Famagusta</option>
+                            </select>
+                        </fieldset>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
