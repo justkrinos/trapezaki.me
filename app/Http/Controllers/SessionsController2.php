5dacfaa957095174d83f469c8b48e40b34fc707c
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-04-09 12:10:49 +0200

    user2 profile tags work
    Co-authored-by: Andreas Efstathiou <Skydrite@users.noreply.github.com>

--- app/Http/Controllers/SessionsController2.php
+++ app/Http/Controllers/SessionsController2.php
@@ -3,32 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\User2;
-use Illuminate\Support\Facades\Auth;  
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 //For USER2
 class SessionsController2 extends Controller
 {
-    public function edit()
+    public function edit(Request $request)
     {
         //Both change password and edit profile are here
         //if(request()->has('form1'))
         //{
         if(request()->has('form1'))
         {
+
+            $request['tags'] = $this->tagsToArray(request()['tags']);
+
             //User3 edit profile
-            $attributes = request()->validate([
-                'description' => 'required|max:1000'
+            $validatedData = $request->validate([
+                'description' => 'required|max:1000',
+
+                'coffee' => 'in:on|required_without_all:food,drinks',
+                'food' => 'in:on|required_without_all:coffee,drinks',
+                'drinks' => 'in:on|required_without_all:coffee,food',
+
+                'tags' => 'required',
+                'tags.*' => 'regex:/^[\pL\s\-]+$/u|max:15'
+
+            ],[
+                'tags.*.regex' => 'Please only use aphabetic characters.'
             ]);
 
-            //how are we gonan change the type?
+            $tags = $validatedData['tags'];
+            unset($validatedData['tags']);
 
-            //get the id of the user
-            $id = request()->validate(['id' => 'required']);
+            //Change the format of type (food,drinks,coffee) using the private function of this object
+            $validatedData = $this->formatType($validatedData);
 
-            //$guest = User3::update($attributes);
-            
-            User2::where('id', $id)->first()->update($attributes);
+
+            $id = Auth::guard('user2')->user()->id;
+            $user = User2::where('id', $id)->first();
+
+            $user->update($validatedData);
+            $user->retag($tags);
+
 
             return redirect('/profile')->with("success", "Your changes have been applied successfully!");
         }
@@ -37,11 +55,6 @@ class SessionsController2 extends Controller
         {
             //get the id of the user
             $id = request()->validate(['id' => 'required', 'username' => 'required']);
-
-            //dd(bcrypt($_POST['old-password']));
-
-            //$_POST["username"] = User3::find($id)->username;
-
 
 
             $oldPassword = request()->validate(['username' => 'required', 'password' => 'required|max:50|min:7']);
@@ -82,6 +95,34 @@ class SessionsController2 extends Controller
         }
     }
 
+    //Function gia xrisi mesa sto object pu kamni format to type (food coffe drinks)
+    private function formatType(array $validatedData){
+        //Kamni ta tis morfis coffee:food:drinks gia osa iparxun
+        //etsi wste na borume na ta kamume extract later
+
+        $stringToMake = ""; //to string pu ena stilume pisw
+        $dataToChange = ['coffee','food','drinks']; //jina p ena checkarume
+
+
+
+        foreach ($dataToChange as $type) { //gia kathe ena p jina p ena checkarume
+            if (array_key_exists($type,$validatedData)){ //an iparxi
+                if (empty($stringToMake))
+                    $stringToMake .= $type; //men tu valis : an akoma en ofkero
+                else
+                    $stringToMake .= ':' . $type; //vartu : afu empike idi ena mesto array
+            }
+            //diagrafw ta data pu mesa gt thelw mono to (type => food:coffee klp)
+            unset($validatedData[$type]);
+        }
+
+        //vallw tu to string p ekama p en ulla mesa
+        $validatedData['type'] = $stringToMake;
+
+        //diw to pisw sto function pu to kalese
+        return $validatedData;
+    }
+
     public function destroy()
     {
         //dd('log the user out');
@@ -115,5 +156,20 @@ class SessionsController2 extends Controller
 
         return redirect('/dashboard')->withInput()->with('success','Welcome back!');
 
+    }
+
+    private function tagsToArray(string $tags){
+        //Asxolithu mono an dennen ofkero, alios aisto na fkalei error sto view
+        if (!empty($tags)) {
+            //En xwrismena se komma, ara kamnw ta se array
+            $tags = explode(',', $tags);
+
+            //To polli 10 tags na mpennun alliws na men mpennei tpt
+            if(count($tags) > 10)
+                $tags = [];
+        }
+        //Stelnw pisw  to array p ekama
+        //gia na to valw mesto request mou gia na ginei to validate
+        return $tags;
     }
 }
