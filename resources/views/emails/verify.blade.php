1ca7ed26803ffd70163448ff47ab760bee155070
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-04-10 09:54:59 +0200

    verification email is ok

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -159,14 +159,14 @@
                                 </tr>
                             </table>
                         </td>
-                    </tr> <!-- COPY -->
+                    </tr>
                     <tr>
                         <td bgcolor="#ffffff" align="left"
                             style="padding: 0px 30px 0px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
                             <p style="margin: 0;">If that doesn't work, copy and paste the following link in your
                                 browser:</p>
                         </td>
-                    </tr> <!-- COPY -->
+                    </tr>
                     <tr>
                         <td bgcolor="#ffffff" align="left"
                             style="padding: 20px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
