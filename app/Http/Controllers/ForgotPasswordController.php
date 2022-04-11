dcb4c0e048bb60775e6b3bf89c86761edd8bb13f
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-04-11 17:38:23 +0200

    (fixed) verify was sent instead of forgot

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -22,7 +22,7 @@ class ForgotPasswordController extends Controller
 
         if($user){
             if(str_ends_with(env('APP_URL'),'.me')) //stelni email mono o server oi sto local
-                 Mail::to($validatedData['email'])->queue(new \App\Mail\MailVerify($user->email, $user->username, $user->verification_code, 'www'));
+                 Mail::to($validatedData['email'])->queue(new \App\Mail\MailForgotPassword($user->email, $user->username, $user->verification_code, 'www'));
         }
 
         return redirect('/login')->with('success', 'An email with password recovery instructions was sent to your inbox!');
