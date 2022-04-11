3ba15b4675ec6caac4067589dbd9cf041f265847
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2022-04-11 13:30:20 +0300

    formatted

--- public/assets/js/active-disable.js
+++ public/assets/js/active-disable.js
@@ -1,83 +1,82 @@
 approvedToast = Toastify({
-    text: 'User is activated!',
+    text: "User is activated!",
     duration: 5000,
-    close:true,
-    gravity:"top",
+    close: true,
+    gravity: "top",
     position: "right",
     backgroundColor: "#3cc2b4",
-})
+});
 
 declinedToast = Toastify({
-    text: 'User is disabled!',
+    text: "User is disabled!",
     duration: 5000,
-    close:true,
-    gravity:"top",
+    close: true,
+    gravity: "top",
     position: "right",
     backgroundColor: "#db0f0f",
-})
+});
 $(document).ready(function () {
-    var parts = window.location.href.split('/');
-    var lastSegment = parts.pop() || parts.pop();  // handle potential trailing slash
+    var parts = window.location.href.split("/");
+    var lastSegment = parts.pop() || parts.pop(); // handle potential trailing slash
 
     console.log(lastSegment);
 
-    $(document).on('click', '.dropdown', function (e) {
-    e.preventDefault();
-    //Get the last 2 segments of URL
-    console.log($("button#dropdownMenuButton").get());
-    var butt = $("button#dropdownMenuButton").get();
-    console.log($(butt).hasClass("btn-danger"));
-    
-        if ($(butt).hasClass("btn-danger")) { //an en to disable button
+    $(document).on("click", ".dropdown", function (e) {
+        e.preventDefault();
+        //Get the last 2 segments of URL
+        console.log($("button#dropdownMenuButton").get());
+        var butt = $("button#dropdownMenuButton").get();
+        console.log($(butt).hasClass("btn-danger"));
+
+        if ($(butt).hasClass("btn-danger")) {
+            //an en to disable button
             $.ajaxSetup({
                 headers: {
-                    'X-CSRF-TOKEN': $('[name="_token"]').attr('value')
-                }
+                    "X-CSRF-TOKEN": $('[name="_token"]').attr("value"),
+                },
             });
-            
+
             var user = lastSegment;
             //Send the request
             $.ajax({
-                url: "/user/"+lastSegment,
-                method: 'post',
+                url: "/user/" + lastSegment,
+                method: "post",
                 data: {
                     username: user, //piase to username attribute
-                    action: 'disable'
+                    action: "disable",
                 },
                 success: function (result) {
                     //if success then continue
-                    console.log(result)
+                    console.log(result);
                     //approvedToast.showToast();
                     declinedToast.showToast();
-                 
-                }
+                },
             });
-        }
-        else if($(butt).hasClass("btn-success")){ //an en to activate button
+        } else if ($(butt).hasClass("btn-success")) {
+            //an en to activate button
             $.ajaxSetup({
                 headers: {
-                    'X-CSRF-TOKEN': $('[name="_token"]').attr('value')
-                }
+                    "X-CSRF-TOKEN": $('[name="_token"]').attr("value"),
+                },
             });
-            
+
             var user = lastSegment;
             //Send the request
             $.ajax({
-                url: "/user/"+lastSegment,
-                method: 'post',
+                url: "/user/" + lastSegment,
+                method: "post",
                 data: {
                     username: user, //piase to username attribute
-                    action: 'activate'
+                    action: "activate",
                 },
                 success: function (result) {
                     //if success then continue
-                   
-                    console.log(result)
+
+                    console.log(result);
                     //approvedToast.showToast();
                     approvedToast.showToast();
-                 
-                }
+                },
             });
         }
-});
+    });
 });
