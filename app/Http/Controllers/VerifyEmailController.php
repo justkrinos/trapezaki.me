b5c6f243bf69df20cc38470c4d9122f5f7eeb092
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-05-01 19:51:46 +0200

    success email msg fixed

--- app/Http/Controllers/VerifyEmailController.php
+++ app/Http/Controllers/VerifyEmailController.php
@@ -12,17 +12,19 @@ class VerifyEmailController extends Controller
     public function verifyUser2($email, $secret)
     {
         $user = User2::where('email', $email)->first();
-        return $this->verify($user,$secret);
+        if (!$user || strcmp($secret, $user->verification_code)) {
+            return abort(404);
+        } else {
+            $user->is_verified = 1;
+            $user->verification_code = substr(md5(rand()), 0, 25); //Create a new verification code
+            $user->save();
+            return redirect('/login')->with('success', 'Your account has been Activated! Our team will contact you for further instructions.');
+        }
     }
 
     public function verifyUser3($email, $secret)
     {
         $user = User3::where('email', $email)->first();
-        return $this->verify($user,$secret);
-    }
-
-    private function verify($user, $secret)
-    {
         if (!$user || strcmp($secret, $user->verification_code)) {
             return abort(404);
         } else {
@@ -32,4 +34,5 @@ class VerifyEmailController extends Controller
             return redirect('/login')->with('success', 'Your account has been Activated! Please log in to continue.');
         }
     }
+
 }
