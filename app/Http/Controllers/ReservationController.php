d7b880ea6cf8535d81204240fb0360ecbe9770fb
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2022-05-02 20:54:09 +0300

    5

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -271,7 +271,7 @@ class ReservationController extends Controller
 
         $user3 = User3::find($reservation->user3_id);
         if(str_ends_with(env('APP_URL'),'.me')) //stelni email mono o server oi sto local
-                        Mail::to($user3->email)->queue(new \App\Mail\MailCancelledReservation
+                        Mail::to($user3->email)->queue(new \App\Mail\MailModifiedReservation
                                                     ($user3->email, $reservation, $user2->business_name));
         //update the reservations details
         $reservation->update($validatedData);
