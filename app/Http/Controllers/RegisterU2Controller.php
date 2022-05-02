dd067b165e30ecc9ca9c47d72611e67b3da5bf70
justkrinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-05-02 17:04:22 +0200

    format type

--- app/Http/Controllers/RegisterU2Controller.php
+++ app/Http/Controllers/RegisterU2Controller.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Time;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Format;
 
 class RegisterU2Controller extends RegisterController
 {
@@ -21,7 +22,7 @@ class RegisterU2Controller extends RegisterController
         $request = request();
         //Split the tags (comma separated) into an array
         //using this object's private function
-        $request['tags'] = $this->tagsToArray($request['tags']);
+        $request['tags'] = Format::tagsToArray($request['tags']);
 
         //First only validate password
         request()->validate([
@@ -80,8 +81,8 @@ class RegisterU2Controller extends RegisterController
             ]
         );
 
-        //Change the format of type (food,drinks,coffee) using the private function of this object
-        $validatedData = $this->formatType($validatedData);
+        //Change the format of type (food,drinks,coffee)
+        $validatedData = Format::formatType($validatedData);
 
 
         //Fefkw pu to a data osa  ennen mesto User2 Table
@@ -135,48 +136,5 @@ class RegisterU2Controller extends RegisterController
 
         return redirect('/login')->with('success', "Your account has been created successfully! Check your email for verification.");
 
-    }
-
-    //Function gia xrisi mesa sto object pu kamni format to type (food coffe drinks)
-    private function formatType(array $validatedData){
-        //Kamni ta tis morfis coffee:food:drinks gia osa iparxun
-        //etsi wste na borume na ta kamume extract later
-
-        $stringToMake = ""; //to string pu ena stilume pisw
-        $dataToChange = ['coffee','food','drinks']; //jina p ena checkarume
-
-
-
-        foreach ($dataToChange as $type) { //gia kathe ena p jina p ena checkarume
-            if (array_key_exists($type,$validatedData)){ //an iparxi
-                if (empty($stringToMake))
-                    $stringToMake .= $type; //men tu valis : an akoma en ofkero
-                else
-                    $stringToMake .= ':' . $type; //vartu : afu empike idi ena mesto array
-            }
-            //diagrafw ta data pu mesa gt thelw mono to (type => food:coffee klp)
-            unset($validatedData[$type]);
-        }
-
-        //vallw tu to string p ekama p en ulla mesa
-        $validatedData['type'] = $stringToMake;
-
-        //diw to pisw sto function pu to kalese
-        return $validatedData;
-    }
-
-    private function tagsToArray(string $tags){
-        //Asxolithu mono an dennen ofkero, alios aisto na fkalei error sto view
-        if (!empty($tags)) {
-            //En xwrismena se komma, ara kamnw ta se array
-            $tags = explode(',', $tags);
-
-            //To polli 10 tags na mpennun alliws na men mpennei tpt
-            if(count($tags) > 10)
-                $tags = [];
-        }
-        //Stelnw pisw  to array p ekama
-        //gia na to valw mesto request mou gia na ginei to validate
-        return $tags;
     }
 }
