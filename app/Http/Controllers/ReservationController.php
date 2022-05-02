ef05a378fd1be711863f314551399b51813e6bfd
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2022-05-02 21:33:36 +0300

    1000

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -119,7 +119,7 @@ class ReservationController extends Controller
                 $reservation = Reservation::create($validatedData);
                 $user2 = User2::find(Table::find($reservation->table_id)->user2_id);
                 Mail::to($user->email)->queue(new \App\Mail\MailCreatedReservation
-                    ($user->email, $validatedData, $user2->business_name));
+                    ($user->email, $reservation, $user2->business_name));
             }
             else
             {
