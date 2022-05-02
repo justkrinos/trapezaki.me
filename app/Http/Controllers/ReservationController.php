3136c9faf1f23fd8a11715c433e7f2d50f06098d
Andreas Efstathiou <95447662+Skydrite@users.noreply.github.com>
2022-05-02 22:15:24 +0300

    1 less

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -226,9 +226,8 @@ class ReservationController extends Controller
 
 
             // return $validatedData;
-            $user3 = User3::where('username', $validatedData['user3_username']);
+            $user3 = User3::where('username', $validatedData['user3_username'])->first();
             $user3_id = $user3->id;
-            return $user3->id;
             $validatedData['user3_id'] = $user3_id;
             unset($validatedData['user3_username']);
 
